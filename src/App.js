90a5516e7256dd4aa8f5d47286a80076f67730ca
Crutiu45 <mareisoa06@gmail.com>
2025-05-16 20:17:17 -0600

    More chnages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import Header from './Components/Header';
-import Main from './Components/Main';
+import Header from './components/Header';
+import Main from './components/Main';
+import "./styles/components/App.scss"
 function App() {
   return (
     <div className='App'>
