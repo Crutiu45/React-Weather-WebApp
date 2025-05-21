f38cd6a139c3f0e90ceab6554117332cd0434a08
Crutiu45 <mareisoa06@gmail.com>
2025-05-21 17:49:22 -0600

    building

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import Header from './components/Header';
 import Main from './components/Main';
 import "./styles/components/App.scss"
+import "bootstrap-icons/font/bootstrap-icons.css";
+
 function App() {
+  const dark = true; //changes theme of App
+  
   return (
-    <div className='App'>
+    <div className={`App-${dark ? 'dark' : 'light'}`}>
       <Header />
       <Main />
     </div>
---
+++ src/Components/Settings.js
@@ -0,0 +1,18 @@
+
+
+function Settings() {
+  return (
+    <div className='Settings'>Settings
+    <div className="theme-toggler">
+      <div className="theme-buttons">
+        <div className="light-theme-btn">
+          <i className="bi bi-sun"></i>
+        </div>
+      </div>
+    </div>
+    <div className="settings-btn"></div>
+    </div>
+  )
+}
+
+export default Settings
