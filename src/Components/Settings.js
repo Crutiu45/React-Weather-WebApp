dcd2f52a6dd64168e1a52f2f974c9458c55e983f
Crutiu45 <mareisoa06@gmail.com>
2025-05-21 18:14:24 -0600

    adjusted

--- src/Components/Settings.js
+++ src/Components/Settings.js
@@ -3,19 +3,19 @@ import '../styles/components/Settings.scss';
 function Settings() {
   return (
     <div className='Settings'>Settings
-    <div className="theme-toggler">
-      <div className="theme-buttons">
-        <div className="light-theme-btn">
-          <i className="bi bi-sun"></i>
-        </div>
-        <div className="dark-theme-btn active">
-          <i className="bi bi-moon"></i>
+      <div className="theme-toggler">
+        <div className="theme-buttons">
+          <div className="light-theme-btn">
+            <i className="bi bi-sun"></i>
+          </div>
+          <div className="dark-theme-btn active">
+            <i className="bi bi-moon"></i>
+          </div>
         </div>
       </div>
-    </div>
-    <div className="settings-btn">
-      <i className="bi bi-gear"></i>
-    </div>
+      <div className="settings-btn">
+        <i className="bi bi-gear"></i>
+      </div>
     </div>
   )
 }
