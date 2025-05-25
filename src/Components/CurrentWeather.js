82562283128aa7f0608930df8999b31363e7e56b
Crutiu45 <mareisoa06@gmail.com>
2025-05-24 20:27:35 -0600

    more

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -2,6 +2,7 @@ import { getCurrentWeather } from "../api";
 
 function CurrentWeather() {
   const data = getCurrentWeather();
+  console.log(data);
   return (
     <div className="CurrentWeather"> </div>
   )
