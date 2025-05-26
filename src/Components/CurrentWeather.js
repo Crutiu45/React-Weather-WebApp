8d98b7413cd2ff39082749ffa17d7f00fd77db8c
Crutiu45 <mareisoa06@gmail.com>
2025-05-26 14:02:46 -0600

    weather icons

--- src/Components/CurrentWeather.js
+++ src/Components/CurrentWeather.js
@@ -2,9 +2,22 @@ import { getCurrentWeather } from "../api";
 
 function CurrentWeather() {
   const data = getCurrentWeather();
-  console.log(data);
+  const {
+    cloud_cover, feels_like, humidity, icon_num, precipitation, summary, temperature, uv_index, visibility, wind,
+  } = data;
+  console.log("icon_num:", icon_num);
   return (
-    <div className="CurrentWeather"> </div>
+    <div className="CurrentWeather"> 
+      <div className="temperature">
+        <div className="weather-icon">
+          <img 
+            src= {`${process.env.PUBLIC_URL}/dist/weather_icons/weather_icons/set01/big/${icon_num}.png`}
+            alt="Summary"
+          />
+        </div>
+      </div>
+      <div className="other-infos"></div>
+    </div>
   )
 }
 
